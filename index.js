3c3a1640ac66fe290f0d00b9d9bf116cb519d80c
Pablo Lee <melon1991@naver.com>
2020-10-11 10:55:36 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Hong:abcd1234@boilerplate.aydfg.mongodb.net/<dbname>?retryWrites=true&w=majority)', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
